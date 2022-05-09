7916be84505e356047853da8cc2138cee3a44736
Deepraj Majalikar <deeprajm35@gmail.com>
2022-05-09 21:31:17 +0530

    Pivot Index

--- Love Babbar java/code/src/BinarySearch/PivotIndex.java
+++ Love Babbar java/code/src/BinarySearch/PivotIndex.java
@@ -11,51 +11,49 @@ public class PivotIndex {
     }
 
     private static int findIndex(int[] nums) {
+        int n = nums.length;
+        int start = 0;
+        int end = n - 1;
+
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            //first line
+            if (nums[mid] >= nums[0]) {
+                start = mid + 1;
+            } else//second line
+            {
+                end = mid;
+            }
+        }
+        return start;//if we do start -1 we will get pivot as 6  // we can return end also
+
+
+
+        //if pivot is 6
 //        int n = nums.length;
 //        int start = 0;
 //        int end = n - 1;
 //
 //        while (start < end) {
 //            int mid = start + (end - start) / 2;
-//            //first line
-//            if (nums[mid] >= nums[0]) {
-//                start = mid + 1;
-//            } else//second line
-//            {
-//                end = mid;
+//            //if mid is greater than mid+1(next of mid) than mid is pivot
+//            if (mid<end && nums[mid]>nums[mid+1]) {
+//                return mid;
 //            }
+//            //if mid is less than mid-1(previous of mid) than mid is pivot
+//            if (mid>0 && nums[mid]<nums[mid-1]) {
+//                return mid-1;
+//            }
+//
+//            if(nums[mid]>=nums[start])
+//            {
+//                start=mid+1;
+//            }
+//            else{
+//                end=mid-1;
+//            }
+//
 //        }
-//        return start;// we can return end also
-
-
-
-        //if pivot is 6
-        int n = nums.length;
-        int start = 0;
-        int end = n - 1;
-
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            //if mid is greater than mid+1(next of mid) than mid is pivot
-            if (mid<end && nums[mid]>nums[mid+1]) {
-                return mid;
-            }
-            //if mid is less than mid-1(previous of mid) than mid is pivot
-            if (mid>0 && nums[mid]<nums[mid-1]) {
-                return mid-1;
-            }
-
-            if(nums[start]>=nums[mid])
-            {
-                //go left
-                end=mid-1;
-            }
-            else{
-                //go right
-                start=mid+1;
-            }
-
-        }
-        return start;
+//        return start;
     }
 }
