5f93030ff4c9a9417110d988929971b80294c873
Deepraj Majalikar <deeprajm35@gmail.com>
2022-06-04 17:35:40 +0530

    recurssion

--- Love Babbar java/code/src/RecursionYT/LetterCombinationsAfAPhoneNumber.java
+++ Love Babbar java/code/src/RecursionYT/LetterCombinationsAfAPhoneNumber.java
@@ -1,25 +1,46 @@
 package RecursionYT;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class LetterCombinationsAfAPhoneNumber {
     public static void main(String[] args) {
-
+        String digits = "23";
+        System.out.println(letterCombinations(digits));
     }
 
     public static List<String> letterCombinations(String digits) {
-        List<String> list = Arrays.asList("","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz");
-        String str="";
         List<String> ans = new ArrayList<>();
-        solve(digits,0,0,list,str,ans);
+
+        if(digits.length() == 0)
+            return ans;
+
+        String[] list = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        StringBuilder output= new StringBuilder("");
+
+        solve(digits,0,list,output,ans);
 
         return ans;
     }
 
-    public static void solve(String digits,int index1,int index2,List<String> list,String str,List<String> ans)
+    public static void solve(String digits,int index,String[] list,StringBuilder output,List<String> ans)
     {
-            
+        //base case
+        if(index == digits.length())
+        {
+            ans.add(String.valueOf(output));
+            return;
+        }
+
+        int num = digits.charAt(index) - '0';
+        String value = list[num];
+
+
+        for (int i = 0; i < value.length() ; i++) {
+            output.append(value.charAt(i));
+            solve(digits,index+1,list,output,ans);
+            output.deleteCharAt(output.length()-1);
+
+        }
     }
 }
