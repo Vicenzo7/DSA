66fc8a1fd11327b036a6a56deb8e47bb141fb389
Deepraj Majalikar <deeprajm35@gmail.com>
2022-06-06 23:16:44 +0530

    recursion

---
+++ Love Babbar java/code/src/RecursionUnacedemy/DecodeString.java
@@ -0,0 +1,52 @@
+package RecursionUnacedemy;
+
+import javax.sound.midi.Soundbank;
+
+//https://leetcode.com/problems/decode-string/
+public class DecodeString {
+    public static void main(String[] args) {
+        String s = "3[a]2[bc]";
+        System.out.println(s);
+        System.out.println(decodeString(s));
+    }
+
+    //pointer
+    static int index = 0;
+    public static String decodeString(String s) {
+        //to store digits;
+        int cur = 0;
+        StringBuilder sb = new StringBuilder();
+
+        while(index<s.length())
+        {
+            char ch = s.charAt(index++);
+            //checking if char is a digit
+            if(ch>= '0' && ch<= '9')
+            {
+                // we are multiplying by 10 because what if there are many digits which makes it two or three digit number
+                cur = cur*10+(ch -'0');
+            }else if(ch == '['){
+                String st = decodeString(s);
+                // add this string cur number of times
+                while(cur >0)
+                {
+                    sb.append(st);
+                    cur --;
+                }
+                cur=0;
+            }
+            // if close bracket found return the words with in the bracket
+            else if(ch == ']')
+            {
+                return sb.toString();
+            }
+            // if not found anything simply append the char to a stringBuilder
+            else{
+                sb.append(ch);
+            }
+        }
+
+        return sb.toString();
+    }
+
+}
---
+++ Love Babbar java/code/src/RecursionUnacedemy/PredictTheWinner.java
@@ -0,0 +1,39 @@
+package RecursionUnacedemy;
+
+public class PredictTheWinner {
+    public static void main(String[] args) {
+        int[] nums = {20,30,2,2,2,10};
+        System.out.println(predictTheWinner(nums));
+    }
+
+    public static boolean predictTheWinner(int[] nums) {
+        int sum = 0;
+
+        for(int k=0;k<nums.length;k++)
+        {
+            sum += nums[k];
+        }
+        System.out.println(sum);
+        int i=0;
+        int j= nums.length-1;
+        int myScore = optimalGameStratergy(nums,i,j);
+        System.out.println( myScore);
+
+
+        int oppScore = sum-myScore;
+        System.out.println(oppScore);
+        return myScore>oppScore;
+    }
+
+    public static int optimalGameStratergy(int[] nums,int i,int j){
+
+        if(i>j)
+            return 0;
+
+        int choice1 = nums[i] + Math.min(optimalGameStratergy(nums,i+2,j),optimalGameStratergy(nums,i+1,j-1));
+
+        int choice2 = nums[j] + Math.min(optimalGameStratergy(nums,i+1,j-1),optimalGameStratergy(nums,i,j-2));
+
+        return Math.max(choice1,choice2);
+    }
+}
