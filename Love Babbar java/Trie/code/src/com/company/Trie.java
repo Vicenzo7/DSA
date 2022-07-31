f1636128e5d7dbae9bfaaed522ecc8674545365a
Deepraj <deeprajm35@gmail.com>
2022-07-31 19:09:28 +0530

    Trie

--- Love Babbar java/Trie/code/src/com/company/Trie.java
+++ Love Babbar java/Trie/code/src/com/company/Trie.java
@@ -7,6 +7,8 @@ public class Trie {
         root = new TrieNode('\0');
     }
 
+    
+    // insert
     void insert(String word){
         insertRec(root,word);
         System.out.println("Insert success");
@@ -28,7 +30,7 @@ public class Trie {
 
         // the alphabet is already present
         if(root.children[index]!=null){
-            // if present to that the child(alphabet)
+            // if present go to that the child(alphabet)
             child = root.children[index];
         }else{ // no such alphabet exist
             //create a child and link it
@@ -55,6 +57,7 @@ public class Trie {
         }
     }
 
+    //search
     boolean search(String word){
         return searchRec(root,word);
     }
@@ -74,7 +77,7 @@ public class Trie {
 
         // the alphabet is already present
         if(root.children[index]!=null){
-            // if present to that the child(alphabet)
+            // if present go to that the child(alphabet)
             child = root.children[index];
         }else{ // no such alphabet exist
             return false;
@@ -83,6 +86,57 @@ public class Trie {
         //recursion
         return searchRec(child,str.substring(1));
 
+    }
+    
+    
+    
+    void delete(String word){
+        deleteRec(root,word);
+    }
+
+    private TrieNode deleteRec(TrieNode root, String str) {
+        // if trie is empty
+        if(root == null)
+            return null;
+
+        //check if last character
+        if(str.length()==0){
+            //check if this is terminal,if yes mark it false as it's no longer terminal
+            if(root.isTerminal){
+                root.isTerminal=false;
+            }
+
+            //check if it has children
+            if(getChildCount(root) == 0){
+                root = null;
+            }
+
+            return root;
+        }
+
+        // if not a last character traverse till the last character
+        char ch = str.charAt(0);
+        int index = ch-'a';
+        root.children[index] = deleteRec(root.children[index],str.substring(1));
+
+        // If root does not have any child (its only child got
+        // deleted), and it is not end of another word.
+        if (getChildCount(root) == 0 && root.isTerminal == false){
+            root = null;
+        }
+
+        return root;
+
+
+    }
+    int getChildCount(TrieNode temp)  {
+        int count = 0;
+
+        for(int i=0; i<26; i++) {
+            if(temp.children[i] != null)
+                count++;
+        }
+        return count;
     }
 
     public static void main(String[] args) {
@@ -93,13 +147,24 @@ public class Trie {
         trie.insert("coder");
         trie.insert("codee");
 
+//        System.out.println(trie.search("code"));
+//        System.out.println(trie.search("coding"));
+//        System.out.println(trie.search("codemonk"));
+//        System.out.println(trie.search("coder"));
+//        System.out.println(trie.search("codee"));
+//        System.out.println(trie.search("codi"));
+//        System.out.println(trie.search("cody"));
+//
+        trie.delete("codemonk");
+        trie.delete("coding");
+        trie.delete("coder");
+        trie.delete("codee");
+
         System.out.println(trie.search("code"));
-        System.out.println(trie.search("coding"));
         System.out.println(trie.search("codemonk"));
+        System.out.println(trie.search("coding"));
         System.out.println(trie.search("coder"));
         System.out.println(trie.search("codee"));
-        System.out.println(trie.search("codi"));
-        System.out.println(trie.search("cody"));
 
     }
 }
