393d32217dfad2226b669d1b493bad8664c93bd4
Deepraj <deeprajm35@gmail.com>
2022-08-12 00:06:18 +0530

    Graph

---
+++ Love Babbar java/Graph/code/src/com/company/ShortestPathInUndirectedGraphs.java
@@ -0,0 +1,81 @@
+package com.company;
+
+import java.util.*;
+import java.util.Queue;
+
+public class ShortestPathInUndirectedGraphs {
+    static void addEdge(ArrayList<ArrayList<Integer>> adj, int u,int v){
+        adj.get(u).add(v);
+        adj.get(v).add(u);
+    }
+
+    static void printGraph(ArrayList<ArrayList<Integer>> adj){
+        for (int i = 0; i < adj.size() ; i++) {
+            System.out.print(i);
+            for (int j = 0; j < adj.get(i).size(); j++) {
+                System.out.print("->"+adj.get(i).get(j));
+            }
+            System.out.println();
+        }
+    }
+
+    public static int[] shortestPath(ArrayList<ArrayList<Integer>> adj,int N , int src){
+
+        int[] distance = new int[N];
+        for (int i = 0; i <N ; i++) {
+            distance[i] = Integer.MAX_VALUE;
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        distance[src]=0;
+        q.add(src);
+
+        while(!q.isEmpty()){
+            int node = q.remove();
+
+            for(int it : adj.get(node)){
+                if(distance[node]+1 < distance[it]){
+                    distance[it]= distance[node]+1;
+                    q.add(it);
+                }
+            }
+        }
+
+        return distance;
+    }
+
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the no of vertices/node");
+        int n = sc.nextInt();
+
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
+        for (int i = 0; i <= n; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        System.out.println("Enter the number of edges for undirected graph");
+        int m = sc.nextInt();
+
+        System.out.println("give the inputs ");
+
+        for (int i = 0; i < m; i++) {
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+
+            addEdge(adj, u, v);
+        }
+        int N = adj.size();
+        System.out.println(Arrays.toString(shortestPath(adj, N, 0)));
+    }
+}
+
+
+
+
+
+
+
