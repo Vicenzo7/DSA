80ad7a9f21b662d126b809bcca11404feca739e8
Deepraj <deeprajm35@gmail.com>
2022-08-21 18:16:07 +0530

    Recursion and Backtracking Series

---
+++ RecurssionAndBacktracking/code/src/com/company/FibonacciSeries.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class FibonacciSeries {
+
+    public static void main(String[] args) {
+        int n = 9;
+        System.out.println(getFib(n));
+        solve(n);
+    }
+
+    private static int getFib(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+
+        return getFib(n -1 ) + getFib(n - 2);
+    }
+
+    private static void solve(int n) {
+        if (n == 0) {
+            return ;
+        }
+
+        System.out.print(n+" ");
+        solve(n -1 ) ;
+        System.out.print(n+" ");
+    }
+}
