df537299da2a72da5a19fe04866fcc99087d9f74
Deepraj <deeprajm35@gmail.com>
2022-10-07 08:57:41 +0530

    Quick Sort

---
+++ Kunal  Kushwa/14-Recursion/code/src/sorting/QuickSort.java
@@ -0,0 +1,48 @@
+package sorting;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class QuickSort {
+    public static void main(String[] args) {
+        int[]arr = {40,12,5,49,86,5,1,78,66};
+        quickSort(arr,0, arr.length-1);
+        System.out.println(Arrays.toString(arr));
+    }
+
+
+    public static void quickSort(int[] arr,int low,int hi){
+
+        if(low>=hi)
+            return;
+
+
+        int start = low;
+        int end = hi;
+        int mid = start + (end-start)/2;
+        int pivot = arr[mid];
+
+        while(start<=end){
+            while(arr[start] < pivot){
+                start++;
+            }
+            while(arr[end] > pivot){
+                end--;
+            }
+
+            if(start<=end){
+                int temp = arr[start];
+                arr[start]=arr[end];
+                arr[end] = temp;
+                start++;
+                end--;
+            }
+        }
+
+        // now my pivot is at correct index, please sort two halves now
+        quickSort(arr, low, end);
+        quickSort(arr, start,hi);
+
+
+    }
+}
