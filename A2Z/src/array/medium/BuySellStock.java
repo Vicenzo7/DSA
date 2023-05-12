2aa30f38193cb1a71111b8689239594245f1c0f6
deeprajm35 <deeprajm35@gmail.com>
2023-05-12 18:50:50 +0530

    Add new example

--- A2Z/src/array/medium/BuySellStock.java
+++ A2Z/src/array/medium/BuySellStock.java
@@ -3,6 +3,7 @@ package array.medium;
 public class BuySellStock {
     public static void main(String[] args) {
         int[] prices = {7, 1, 5, 3, 6, 4};
+        //int[] prices = {100, 180, 260, 310, 40, 535, 695};
         System.out.println(maxProfit(prices));
         System.out.println(maxProfitOptimal(prices));
         stockBuySell(prices,prices.length);
