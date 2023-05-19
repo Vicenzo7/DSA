abffa0d70c800246828c1f8f7f15b9b34da739b3
deeprajm35 <deeprajm35@gmail.com>
2023-05-20 00:08:05 +0530

    Check array sorted using BinarySearch and Find Insert position of element using binary search

---
+++ A2Z/src/binarysearch/CheckIfArraySorted.java
@@ -0,0 +1,54 @@
+package binarysearch;
+
+public class CheckIfArraySorted {
+    public static void main(String[] args) {
+        int[] nums = {10, 20, 30, 40, 50};
+        int[] nums2 = {90, 3, 8, 10, 8, 9, 2};
+
+        System.out.println(binarySearch(nums2, 0, nums2.length - 1));
+    }
+
+
+    /*
+        Brute for approach
+        Run a loop and check if there exit this condition if nums[i] > nums[i+1]
+        This the above condition exist then the array is not sorted else it's sorted
+
+        TC = O(n) to traverse the array
+
+
+        How about to do it using binarySearch
+        Things to note
+        1. A single element is always sorted
+
+        Using recursion
+        steps1: if an element is alone than its sorted, return true
+        steps2 : check left part is sorted, leftPart = low to mid
+        steps3 : check right part is sorted, rightPart = mid+1 to high
+        steps4 : Check if the current element is sorted, nums[mid] < nums[mid +1]
+
+        Step5 : Return the result of the three
+
+        TC = O(log(n))
+        SC = O(log(n)) -> Auxiliary space
+
+    */
+
+
+    public static boolean binarySearch(int[] nums, int left, int right) {
+        if (left == right) {
+            return true; // A single element is always sorted
+        }
+
+        int mid = left + (right - left) / 2;
+
+        boolean isLeftPartSorted = binarySearch(nums, left, mid);
+        boolean isRightPartSorted = binarySearch(nums, mid + 1, right);
+
+        // check if the current element is sorted
+        boolean isCurrentElementSorted = nums[mid] <= nums[mid + 1];
+
+
+        return isLeftPartSorted && isCurrentElementSorted && isRightPartSorted;
+    }
+}
---
+++ A2Z/src/binarysearch/SearchInsertPosition.java
@@ -0,0 +1,56 @@
+package binarysearch;
+
+public class SearchInsertPosition {
+    public static void main(String[] args) {
+        /*Example 1:
+
+        Input: nums = [1,3,5,6], target = 5
+        Output: 2
+
+        Example 2:
+        Input: nums = [1,3,5,6], target = 2
+        Output: 1
+
+        Example 3:
+        Input: nums = [1,3,5,6], target = 7
+        Output: 4*/
+
+        int[] nums = {1, 3, 5, 6};
+        int target = 7;
+
+        System.out.println(searchInsert(nums,target));
+    }
+
+    /*
+        Solution - BinarySearch
+
+        Since the array is sorted apply a binary search,
+        if we find the target we return its index or the index where it could have been placed
+
+        Step1 : run the loop while low <= high
+        if nums[mid] == target return mid;
+        else if nums[mid] < target -> low = mid+1
+        else if nums[mid] > target -> high = mid-1;
+
+        if we don't find the element just return the low pointer that will be the index at which we need to insert the target
+
+    */
+
+    public static int searchInsert(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return low;
+    }
+}
